0237d35de5e9500e753c9f403a945264caf36eed
andrew otski <andrew.k.otsuki@gmail.com>
2015-09-24 15:03:52 -0400

    Class handData added, has no functions just a structure/object used to hold valuable hand data for comparisions

---
+++ Assignment1_Comp4004_AndrewOtsuki_100818918/src/JunitTests/HandData.java
@@ -0,0 +1,12 @@
+package JunitTests;
+
+public class HandData {
+	public int      flush = -1,
+					straight = -1,
+					fourOAK = -1,
+					threeOAK = -1,
+					pair1 = -1,
+					pair2 = -1,
+					handVal = -1,
+					highCard = -1;
+}
