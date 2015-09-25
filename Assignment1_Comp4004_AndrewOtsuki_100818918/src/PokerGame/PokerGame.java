f2f382e318081e7993f91b88843f7b0bc0b1a2c8
andrew otski <andrew.k.otsuki@gmail.com>
2015-09-24 22:46:36 -0400

    added functionality for flipPlayers() tests now pass

--- Assignment1_Comp4004_AndrewOtsuki_100818918/src/PokerGame/PokerGame.java
+++ Assignment1_Comp4004_AndrewOtsuki_100818918/src/PokerGame/PokerGame.java
@@ -7,7 +7,10 @@ public class PokerGame {
 	
 	//flips the position in array of players at player1,player2
 	public void flipPlayers(int player1, int player2)
-	{		
+	{
+		PokerPlayer temp = players[player1];
+		players[player1] = players[player2];
+		players[player2] = temp;		
 	}
 	
 }
